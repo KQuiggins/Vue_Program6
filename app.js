f67368b45aa3c0af6d650f9b4983dc7edca7e78c
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-11-17 14:12:03 -0500

    created table and rows

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
     data() {       
         return {
+            tickets: [],
+            
             
         }
-    }
+    },
+    created() {
+        this.getTickets();
+    },
+    methods: {
+       async getTickets() {
+            const response = await fetch('tickets1.json');
+            this.tickets = await response.json();
+            
+        }
+    },
 })
 
 app.mount('#main')   // mount in div id=template
