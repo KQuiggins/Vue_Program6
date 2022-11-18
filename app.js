802dda7a73b511f1844f71bfb101d68d44c09a03
KQuiggins <kashfamilyvalues@gmail.com>
2022-11-17 19:30:41 -0500

    filtered data for 25 over table

--- app.js
+++ app.js
@@ -2,19 +2,26 @@ const app = Vue.createApp({
     data() {       
         return {
             tickets: [],
+            filtered: [],
+            //showTable: false,
+
             
             
         }
     },
     created() {
         this.getTickets();
+       
     },
     methods: {
        async getTickets() {
             const response = await fetch('tickets1.json');
             this.tickets = await response.json();
+            this.filtered = this.tickets.filter((ticket) => ticket.actual_speed - ticket.posted_speed >= 25);
+            console.log(this.filtered); 
             
-        }
+        },
+        
     },
 })
 
